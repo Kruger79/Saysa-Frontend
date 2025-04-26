b50bbbde2af39891c76cb6334fefa32808913987
Kruger79 <kevingonzalezballadares@gmail.com>
2025-04-25 20:58:14 -0600

    Rutas nuevas para la vista perfil y la vista administrador

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../views/Home';
 import Login from '../views/Login';
 import Register from '../views/Register';
+import Perfil from '../views/Perfil';
+import AdminDashboard from '../views/AdminDashboard';
 
 export default function AppRouter() {
   return (
@@ -10,6 +12,8 @@ export default function AppRouter() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </BrowserRouter>
   );
