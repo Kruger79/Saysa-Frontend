c8ce8535fe1a81e353758ce1574b9f017fac490a
Kruger79 <kevingonzalezballadares@gmail.com>
2025-04-27 00:05:40 -0600

    Vista general de la lista de productos

---
+++ src/views/Products.jsx
@@ -0,0 +1,40 @@
+import Navbar from "../components/Navbar";
+import ProductoCard from "../components/ProductoCard";
+import "../../public/css/Products.css";
+
+import { useEffect, useState } from "react";
+import { obtenerProductos } from "../api/productos";
+
+export default function Productos() {
+    const [productos, setProductos] = useState([]);
+  
+    useEffect(() => {
+      async function cargarProductos() {
+        const data = await obtenerProductos();
+        setProductos(data);
+      }
+      cargarProductos();
+    }, []);
+  
+    return (
+      <div className="productos-page">
+        <Navbar />
+  
+        <div className="productos-banner">
+          <div className="banner-content">
+            <h1 className="banner-title">Productos para entrega inmediata</h1>
+            <p className="banner-subtitle">
+              Estos productos se encuentran en nuestro almacén con cantidades limitadas.
+            </p>
+          </div>
+        </div>
+  
+        {/* Contenedor de productos */}
+        <div className="productos-container">
+          {productos.map((producto, index) => (
+            <ProductoCard key={producto.IdProducto} producto={producto} reverse={index % 2 !== 0} />
+          ))}
+        </div>
+      </div>
+    );
+  }
